415644a38c0f4d098584c7f2cc85bcdcaeed8eef
Pedro  Paulo Lima Dias <pedroppld@gmail.com>
2024-08-10 19:25:16 -0300

    1 a 5

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['img.freepik.com','images.unsplash.com'],
+    domains: ['img.freepik.com','images.unsplash.com', "deadtalknews.com","as1.ftcdn.net","occ-0-2794-2219.1.nflxso.net" ,"images.vexels.com", "png.pngtree.com"],
   }
 };
 
