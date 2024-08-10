9d94616e6c14cebef1af1a7af506bf39db802ed6
Pedro  Paulo Lima Dias <pedroppld@gmail.com>
2024-08-10 18:11:14 -0300

    foto

--- src/app/Details/page.jsx
+++ src/app/Details/page.jsx
@@ -9,7 +9,7 @@ export default function Details() {
 
 
   return (
-    <div className="relative h-screen w-full flex items-center justify-center ">
+    <div className=" h-screen w-full flex items-center justify-center ">
     <Image className='' src={'https://img.freepik.com/fotos-premium/personagem-assustador-de-pessoa-posando-em-vestes-pretas_1007695-382.jpg'} layout="fill" objectFit="cover" alt='' />
     <div className="absolute inset-0 flex items-center justify-center">
         <div className=' bg-gray-950 backdrop-blur-xl  bg-opacity-10 p-8 flex align-center text-center'>
